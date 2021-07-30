c0836184862cd320c0339b57af8595a0af5d33b3
Vicente Soriano <vsoriano58@gmail.com>
2021-07-30 18:56:24 +0200

    80. Animaciones por CSS en nuestra aplicación

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -9,9 +9,9 @@ export default function GifGrid({ category }) {
 
     return (
         <>
-            <h3>{ category }</h3>
+            <h3 className="animate__animated animate__fadeIn">{ category }</h3>
 
-            { loading && <p>Loading</p>}
+            { loading && <p className="animate__animated animate__flash">Loading</p>}
 
             <div className="card-grid">
                 
