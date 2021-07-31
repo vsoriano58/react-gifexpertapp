a972c51ec41f7ef83e8bf0686ca692a99a1b7576
Vicente Soriano <vsoriano58@gmail.com>
2021-07-31 10:18:42 +0200

    docs agregado

---
+++ docs/precache-manifest.dbac3763d93142cb38988f97c7261c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc18194e51956673bdba4f355d53c812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3612709d6e9a11b1563d",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "069fa069928c4ced8259",
+    "url": "/static/js/2.d5de64be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5de64be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3612709d6e9a11b1563d",
+    "url": "/static/js/main.72537173.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
